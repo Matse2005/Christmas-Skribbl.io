653bdfad512d606e62c0c9e890d092c48ed800fa
MatseVH <matse.vanhorebeek@gmail.com>
2022-11-06 15:53:23 +0100

    0.0.1-beta - Commit 2

--- firefox/scripts/run.js
+++
@@ -1,7 +0,0 @@
-let msgAnnounce = document.querySelector(".js-badge-msg");
-let msgMessageButton = document.querySelector(".js-btn-messages");
-msgAnnounce.innerHTML = Number(parseInt(msgAnnounce.innerHTML));
-if (isNaN(parseInt(msgAnnounce.innerHTML)))
-  msgAnnounce.innerHTML = "Blijf eens van de Inspector af :-)";
-else msgAnnounce.innerHTML = (parseInt(msgAnnounce.innerHTML) + 1).toString();
-msgAnnounce.removeAttribute("hidden");
---
+++ run.js
@@ -0,0 +1,9 @@
+document.body.style.background =
+  "url('https://raw.githubusercontent.com/Matse2005/Christmas-Skribbl.io/master/assets/background.png')";
+
+document.body.innerHTML = document.body.innerHTML.replace(
+  /logo.gif/g,
+  "https://raw.githubusercontent.com/Matse2005/Christmas-Skribbl.io/master/assets/logo.png"
+);
+
+document.body.style.border = "5px solid red";
